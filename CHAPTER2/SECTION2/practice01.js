838bdc0f4fc516fe6e8261ebf8e102dca084d885
leno41bo52ru <leno41bo52ru@gmail.com>
2020-03-22 21:56:33 +0900

    20200322bk
    htmlの操作まで

---
+++ CHAPTER2/SECTION2/practice01.js
@@ -0,0 +1,32 @@
+// $(function() {
+//   $('input').val('値を変更しました')
+// });
+
+// $(function() {
+//   $('#inputTxt').val('値を変更しました');
+// });
+
+// $(function() {
+//   $('div > input').val('値を変更しました');
+// });
+
+// $(function() {
+//   $('input[name=title]').val('値を変更しました');
+// });
+
+// $(function() {
+//     $('input:text').val('値を変更しました');
+// });
+
+// $(function() {
+//     $('li:first input').val('値を変更しました');
+//     $('li:nth-Child(3) input').val('値を変更しました');
+// });
+
+// $(function() {
+//     $('div:has(p)').css('border', '1px solid #ff0000');
+// });
+
+$(function() {
+    ('input:visible').value('値を変更しました');
+});
---
+++ CHAPTER2/SECTION2/practice02.js
@@ -0,0 +1,12 @@
+// $(function() {
+//     $('#txt').text('テキストが変更されました');
+// });
+
+// $(function() {
+//     $('#contents').html('<p id="txt">p要素が追加されました。</p>');
+
+// });
+
+$(function() {
+    $('#txt').remove();
+});
